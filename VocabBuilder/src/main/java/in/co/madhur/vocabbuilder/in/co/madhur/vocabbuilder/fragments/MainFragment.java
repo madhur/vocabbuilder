908f17c1b4eb3fae54a57758507ee7005852f82f
Madhur <ahuja.madhur@gmail.com>
2014-06-19 20:20:16 +0530

    Initial commit

---
+++ VocabBuilder/src/main/java/in/co/madhur/vocabbuilder/in/co/madhur/vocabbuilder/fragments/MainFragment.java
@@ -0,0 +1,9 @@
+package in.co.madhur.vocabbuilder.in.co.madhur.vocabbuilder.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by madhur on 19-Jun-14.
+ */
+public class MainFragment extends Fragment {
+}
---
+++ VocabBuilder/src/main/java/in/co/madhur/vocabbuilder/in/co/madhur/vocabbuilder/fragments/SettingsFragment.java
@@ -0,0 +1,9 @@
+package in.co.madhur.vocabbuilder.in.co.madhur.vocabbuilder.fragments;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by madhur on 19-Jun-14.
+ */
+public class SettingsFragment extends FragmentActivity {
+}
